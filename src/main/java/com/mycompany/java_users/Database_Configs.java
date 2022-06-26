dcef3611483b1460bd806fc47b0c3dbdcc9939ab
Gustavo <gustavohs2004@gmail.com>
2022-06-26 18:42:49 -0300

    Definindo string de conexao do banco

---
+++ src/main/java/com/mycompany/java_users/Database_Configs.java
@@ -0,0 +1,19 @@
+package com.mycompany.java_users;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database_Configs {
+
+    private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+    private static final String DATABASE_NAME = "a3_unis";
+    private static final String DATABASE_URL = "jdbc:mysql://host.docker.internal:3306/";
+    private static final String DATABASE_USER = "root";
+    private static final String DATABASE_PASSWORD = "1234";
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(DRIVER_NAME);
+        return DriverManager.getConnection(DATABASE_URL + DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD);
+    }
+}
---
+++ src/main/java/com/mycompany/java_users/Database_Operations.java
@@ -0,0 +1,5 @@
+package com.mycompany.java_users;
+
+public class Database_Operations {
+    
+}
