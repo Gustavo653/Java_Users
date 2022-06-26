44cb195aa00ab5db5deb2960c088daa252e1b4a4
Gustavo <gustavohs2004@gmail.com>
2022-06-26 18:49:48 -0300

    Criando metodos de interacao com o banco

--- src/main/java/com/mycompany/java_users/Java_Users.java
+++ src/main/java/com/mycompany/java_users/Java_Users.java
@@ -3,6 +3,10 @@ package com.mycompany.java_users;
 public class Java_Users {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Database_Operations databaseOperations = new Database_Operations();
+        databaseOperations.read();
+        databaseOperations.insert();
+        databaseOperations.update();
+        databaseOperations.delete();
     }
 }
